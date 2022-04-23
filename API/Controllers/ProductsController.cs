9f1e913d76835d858fc223f176323831832b3f41
Ceco Svidovski <ceco_svidovski@abv.bg>
2022-04-23 23:40:29 +0300

    Fix products controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -26,6 +26,10 @@ public class ProductsController : ControllerBase
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        return await _context.Products.FindAsync(id);
+        var product = await _context.Products.FindAsync(id);
+
+        if (product == null) return NotFound();
+
+        return product;
     }
 }
