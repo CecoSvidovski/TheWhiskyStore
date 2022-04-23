23dfa947805e3e4614efe5ad72c61367f3aa5ec2
Ceco Svidovski <ceco_svidovski@abv.bg>
2022-04-23 22:32:51 +0300

    Add controller that generates errors

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ErrorController : ControllerBase
+{
+    [HttpGet("not-found")]
+    public ActionResult GetNotFound()
+    {
+        return NotFound();
+    }
+
+    [HttpGet("bad-request")]
+    public ActionResult GetBadRequest()
+    {
+        return BadRequest(new ProblemDetails{Title = "This is a bad request"});
+    }
+    
+    [HttpGet("unauthorized")]
+    public ActionResult GetUnauthorized()
+    {
+        return Unauthorized();
+    }
+
+    [HttpGet("validation-error")]
+    public ActionResult GetValidationError()
+    {
+        ModelState.AddModelError("ValidationError1", "This is the first error");
+        ModelState.AddModelError("ValidationError2", "This is the second error");
+
+        return ValidationProblem();
+    }
+
+    [HttpGet("server-error")]
+    public ActionResult GetServerError()
+    {
+        throw new Exception("This is a server error");
+    }
+
+    
+
+}
